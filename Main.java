a6da9af211ac77e429cf81687253196ccd0884e2
Your Name <you@example.com>
2024-07-14 21:19:57 +0800

    Compile Pengguna dan Tamu

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-    String variabelString;
+    
     Pengguna pengguna = new Pengguna();
+        pengguna.setId(3);
+        System.out.println(pengguna.getid());
+        pengguna.login();
+        pengguna.daftar();
+        pengguna.setUsername("GIlang");
+        System.out.println(pengguna.getUsername());
+        pengguna.setPassword("Admin#1234");
+        System.out.println(pengguna.getPassword());
+
+        Tamu tamu = new Tamu();
+        tamu.menambahMasaAktif(5);
+        tamu.login();
+        tamu.hapus();
+
+        
     }
 }
