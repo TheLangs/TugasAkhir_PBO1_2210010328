dd1744374d9dded4369fe75e883dfcf4432fa139
Your Name <you@example.com>
2024-07-14 20:47:47 +0800

    menambahkan setter

--- Pengguna.java
+++ Pengguna.java
@@ -7,6 +7,10 @@ public class Pengguna {
         System.out.println("Object telah diciptakan, contructor berjalan\n");
     }
     
+    public void setId(int id){
+    this.id = id;
+    }
+
     public int getid(){
         return this.id;
     }
@@ -15,16 +19,30 @@ public class Pengguna {
     System.out.println("Ini method Daftar user baru\n");
     }
 
+    public void setUsername (String username){
+        this.username = username;
+    }
+
     public String getUsername() {
     return this.username;
     }
 
+    public void setPassword(String password){
+        this.password = password;   
+    }
 
     public String getPassword(){
         return this.password;
     }
 
+    public void hapus(){
+    System.out.println("DELETE FROM pengguna\n");
+    }
 
+    public void hapus(String username){
+    System.out.println("DELETE FROM pengguna WHERE username = "+username+";\n");
+    }
+    
     public void login(){
     System.out.println("Ini method untuk Login");
     }
